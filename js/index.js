434acb2262a8630956cb4daee9df086e7a98f676
SanaChinaliyeva <sana.chinaliyeva@gmail.com>
2019-01-19 12:59:47 +0600

    Added my feed with posts

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-var email = "schinaliyeva@gmail.com";
+var email = "lolo@pepe.com";
 var lastDate = "";
-var baseUrl = "http://146.185.154.90:8000/blog/" + email + "/profile";
+var baseUrl = "http://146.185.154.90:8000/blog/" + email;
 
 var convertFormToObj = function (form) {
   var data = $(form).serializeArray();
@@ -22,6 +22,7 @@ var editProfile = function (userInfo) {
 var showProfile = function (user) {
   var name = user.firstName + " " + user.lastName;
   $("#user").html(name);
+  return user;
 };
 
 $("#editProfile").on("submit", function (e) {
@@ -31,5 +32,32 @@ $("#editProfile").on("submit", function (e) {
   editProfile(userInfo);
 });
 
-getProfile(baseUrl)
-.then(showProfile);
+var getPosts = function () {
+  var url = baseUrl + "/posts";
+  var posts = getProfile(url);
+  console.log(posts);
+  return posts;
+};
+
+var markupPosts = function (post) {
+  var name = post.user.firstName + " " + post.user.lastName;
+  var card = `<div class="card">
+                <div class="card-body">
+                  <h5 class="card-title"><span>${name}</span><span> said:</span></h5>
+                  <p class="card-text">${post.message}</p>
+                </div>
+              </div>`;
+  $("#feed").prepend(card);
+};
+
+var showPosts = function (posts) {
+  for (var post of posts) {
+    markupPosts(post);
+  }
+  return posts;
+};
+
+getProfile(baseUrl + "/profile")
+.then(showProfile)
+.then(getPosts)
+.then(showPosts);
